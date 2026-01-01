d51d0a1882f81f78d55ce7048101ce7c7ae8b1fa
26Labs <ethan.cohen918@gmail.com>
2026-01-01 07:52:28 -0500

    Initial Commit

---
+++ Alfred App/Alfred_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alfred_AppApp.swift
+//  Alfred App
+//
+//  Created by Ethan on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Alfred_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
