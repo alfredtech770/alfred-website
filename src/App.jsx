713bf6f1052b7913db9dbcf7fd167f58b92f8151
alfredtech770 <ethan@alfredconcierge.app>
2026-04-13 13:21:28 -0400

    fix: restore Terms, Privacy, BlogPost, FeaturedEvents imports and routes

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,8 @@ import HotelsPage from "./pages/HotelsPage";
 import HotelDetailPage from "./pages/HotelDetailPage";
 import AdminPage from "./pages/AdminPage";
 import ProposalBuilderPage from "./pages/ProposalBuilderPage";
+import TermsPage from "./pages/TermsPage";
+import PrivacyPage from "./pages/PrivacyPage";
 
 var sf = function(size, weight){
   return {fontFamily:"-apple-system, 'SF Pro Display', 'Helvetica Neue', sans-serif", fontSize:size, fontWeight:weight||400, WebkitFontSmoothing:"antialiased"};
@@ -436,6 +438,8 @@ export default function App(){
         <Route path="/city/:slug" element={<CityPage/>}/>
         <Route path="/catalog/hotels" element={<HotelsPage/>}/>
         <Route path="/catalog/hotels/:slug" element={<HotelDetailPage/>}/>
+        <Route path="/terms" element={<TermsPage/>}/>
+        <Route path="/privacy" element={<PrivacyPage/>}/>
         <Route path="/admin" element={<AdminPage/>}/>
         <Route path="/proposal" element={<ProposalBuilderPage/>}/>
         <Route path="*" element={<NotFoundPage/>}/>
